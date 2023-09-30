d3a0b2b725a0631dfb4c8f46df9084ee70da9e0b
이윤표 <@naver.com>
2023-09-30 17:17:14 +0900

    feat: GlobalStyle 적용

--- my-capstone/src/App.tsx
+++ my-capstone/src/App.tsx
@@ -1,8 +1,10 @@
 import Header from './components/Header';
+import { GlobalStyle } from './styles/globalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
     </>
   );
