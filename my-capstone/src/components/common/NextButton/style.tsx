fd9c555607064369fa145fc7d48d50a85f3fbe9e
이윤표 <@naver.com>
2023-09-30 22:00:18 +0900

    feat: 화살표 버튼 컴포넌트 생성

---
+++ my-capstone/src/components/common/NextButton/style.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+interface Props {
+  width: number;
+  height: number;
+  isShow: boolean;
+}
+
+export const Container = styled.div<Props>`
+  ${({ width, height, isShow }) => {
+    return css`
+      width: ${width};
+      height: ${height};
+
+      visibility: ${isShow ? 'visible' : 'hidden'};
+    `;
+  }}
+`;
