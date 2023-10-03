5c94222d1eb837756265814c978c5e3d592ce9f0
이윤표 <@naver.com>
2023-10-04 03:44:58 +0900

    feat: 제품 이미지 캐러셀 적용

--- my-capstone/src/components/Article/ItemImage/index.tsx
+++ my-capstone/src/components/Article/ItemImage/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
-import { NextButton } from '@/components/common/NextButton';
 import { PrevButton } from '@/components/common/PrevButton';
+import { NextButton } from '@/components/common/NextButton';
 
 import * as S from './style';
 
@@ -10,27 +10,39 @@ interface Props {
 }
 
 export function ItemImage({ images }: Props) {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideRef = useRef<HTMLDivElement | null>(null);
 
   const goNextImage = () => {
-    setCurrentImage(prev => (prev === images.length - 1 ? 0 : prev + 1));
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
   };
 
   const goPriviousImage = () => {
-    setCurrentImage(prev => (prev === 0 ? images.length - 1 : prev - 1));
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
   };
 
   const goSpecificImage = (index: number) => {
-    setCurrentImage(index);
+    setCurrentIndex(index);
   };
+
+  useEffect(() => {
+    if (slideRef.current) {
+      slideRef.current.style.transition = 'all 0.4s ease-in-out';
+      slideRef.current.style.transform = `translateX(-${(currentIndex * 100) / images.length}%)`;
+    }
+  }, [currentIndex]);
 
   return (
     <S.Container>
       <S.Wrapper>
         <PrevButton width="48" height="48" onClick={goPriviousImage} isShow={images.length !== 1} />
-        <S.ImageBox>
+        <S.ImageBox ref={slideRef}>
           {images.map((imageSrc, index) => (
-            <S.Image src={imageSrc} isCurrent={index === currentImage} />
+            <S.Image key={index} imgUrl={imageSrc} />
           ))}
         </S.ImageBox>
         <NextButton width="48" height="48" onClick={goNextImage} isShow={images.length !== 1} />
@@ -38,7 +50,7 @@ export function ItemImage({ images }: Props) {
           {images.length !== 1 && (
             <S.Dots>
               {images.map((_, index) => (
-                <S.Dot isCurrent={index === currentImage} onClick={() => goSpecificImage(index)} />
+                <S.Dot key={index} isCurrent={index === currentIndex} onClick={() => goSpecificImage(index)} />
               ))}
             </S.Dots>
           )}
--- my-capstone/src/components/Article/ItemImage/style.tsx
+++ my-capstone/src/components/Article/ItemImage/style.tsx
@@ -1,42 +1,54 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-interface ImageProps {
+interface DotProps {
   isCurrent: boolean;
 }
 
-export const Container = styled.div``;
+interface ImageProps {
+  imgUrl: string;
+}
+
+interface ArrowButtonProps {
+  isShow: boolean;
+}
+
+export const Container = styled.div`
+  width: 550px;
+  height: 500px;
+  margin: 0 auto;
+`;
 
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
 
   position: relative;
+
+  overflow: hidden;
 `;
 
 export const ImageBox = styled.div`
-  width: 600px;
-  height: 450px;
-
   display: flex;
+  position: relative;
 `;
 
-export const Image = styled.img<ImageProps>`
-  ${({ isCurrent }) => {
+export const Image = styled.div<ImageProps>`
+  ${({ imgUrl }) => {
     return css`
-      width: 100%;
-      height: 100%;
+      width: 550px;
+      height: 500px;
 
       border-radius: 10px;
-
-      display: ${isCurrent ? 'block' : 'none'};
+      background-position: center;
+      background-image: url(${imgUrl});
+      background-size: cover;
     `;
   }}
 `;
 
 export const DotBox = styled.div`
-  width: 600px;
-  left: 48px;
+  width: 550px;
   bottom: 15px;
 
   display: flex;
@@ -48,17 +60,19 @@ export const DotBox = styled.div`
 
 export const Dots = styled.ul`
   padding: 7px 10px;
-  gap: 8px;
+  gap: 9px;
 
   display: flex;
+
   border-radius: 99px;
 
+  transition: background-color 0.2s ease-in-out;
   &:hover {
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.6);
   }
 `;
 
-export const Dot = styled.li<ImageProps>`
+export const Dot = styled.li<DotProps>`
   ${({ isCurrent }) => {
     return css`
       width: 10px;
