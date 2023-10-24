6190c8a3d4b41ba0d1c63ce835162d518f7bdeba
이윤표 <@naver.com>
2023-10-24 19:44:32 +0900

    #10 chore: calculateDaysAgo 유틸함수 등록

---
+++ RN-mobile/src/utils/index.ts
@@ -0,0 +1,31 @@
+import { TIME_UNITS } from "../constants";
+
+export function calculateDaysAgo(inputDate: string) {
+    const now = new Date().getTime();
+    const inputDateTime = new Date(inputDate).getTime();
+    const timeDiff = now - inputDateTime;
+  
+    const secondsAgo = Math.floor(timeDiff / TIME_UNITS.millisecond);
+    const minutesAgo = Math.floor(secondsAgo / TIME_UNITS.seconds);
+    const hoursAgo = Math.floor(minutesAgo / TIME_UNITS.minutes);
+    const daysAgo = Math.floor(hoursAgo / TIME_UNITS.hours);
+    const weeksAgo = Math.floor(daysAgo / TIME_UNITS.weeks);
+  
+    if (weeksAgo > 0) {
+      return `${weeksAgo}주 전`;
+    }
+    if (daysAgo > 0) {
+      return `${daysAgo}일 전`;
+    }
+    if (hoursAgo > 0) {
+      return `${hoursAgo}시간 전`;
+    }
+    if (minutesAgo > 0) {
+      return `${minutesAgo}분 전`;
+    }
+    if (secondsAgo > 0) {
+      return `${secondsAgo}초 전`;
+    } 
+  
+    return '방금 전';
+  }
