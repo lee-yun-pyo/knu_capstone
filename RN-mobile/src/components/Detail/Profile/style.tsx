b9eff39c721816d7ee6d5fc94e3ed445618cb135
이윤표 <@naver.com>
2023-10-25 18:51:45 +0900

    #10 feat: Detail 페이지 프로필 컴포넌트 UI 구현

---
+++ RN-mobile/src/components/Detail/Profile/style.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+  },
+  profileText: {
+    gap: 3,
+  },
+  storeName: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  location: {
+    fontSize: 14,
+    color: "#A4A5A1",
+  },
+});
