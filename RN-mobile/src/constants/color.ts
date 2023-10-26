33ab4cf62bc243fcfd95d4b561bc377d485ccd63
이윤표 <@naver.com>
2023-10-26 18:45:26 +0900

    #10 feat: 가게 위치 & Footer UI 구현

--- RN-mobile/src/constants/color.ts
+++ RN-mobile/src/constants/color.ts
@@ -1,5 +1,17 @@
 export const FontColor = {
     gray: "#A4A5A1",
     startPriceColor: "#3983F6",
-    upperPriceColor: "#ee4638"
+    upperPriceColor: "#ee4638",
+}
+
+export const BackGroundColor = {
+    GREEN: "#03C75A",
+}
+
+export const Border = {
+    LIGHT_MODE: "#E2E2E2",
+}
+
+export const Etc = {
+    LIKE: "#F14B58",
 }
